7776263ad250748d7645e369ffe10f62caf09756
yugyesh <uges.kk@gmail.com>
2021-10-29 18:32:02 +0900

    feat: Add queries for staging tables

--- src/sql_queries.py
+++ src/sql_queries.py
@@ -1,3 +1,5 @@
+from src.constants import DWH_ROLE_ARN
+
 # region drop table queries
 drop_staging_songs = "DROP TABLE IF EXISTS staging_songs"
 drop_staging_events = "DROP TABLE IF EXISTS staging_events"
@@ -8,39 +10,47 @@ drop_artists = "DROP TABLE IF EXISTS artists"
 drop_users = "DROP TABLE IF EXISTS users"
 drop_time = "DROP TABLE IF EXISTS time"
 
-drop_table_queries = [drop_songplay, drop_songs, drop_artists, drop_users, drop_time]
+drop_table_queries = [
+    drop_songplay,
+    drop_songs,
+    drop_artists,
+    drop_users,
+    drop_time,
+    drop_staging_songs,
+    drop_staging_events,
+]
 
 # region create table queries
 create_staging_songs = """
 CREATE TABLE IF NOT EXISTS staging_songs(
     num_songs INT PRIMARY KEY,
-    artist_id VARCHAR(25) NOT NULL,
+    artist_id VARCHAR(100) NOT NULL,
     artist_latitude FLOAT,
     artist_longitude FLOAT,
-    artist_location VARCHAR(100) NOT NULL,
-    artist_name VARCHAR(50) NOT NULL,
-    song_id VARCHAR(25) NOT NULL,
-    title VARCHAR(50) NOT NULL,
+    artist_location VARCHAR(80) NOT NULL,
+    artist_name VARCHAR(100) NOT NULL,
+    song_id VARCHAR(100) NOT NULL,
+    title VARCHAR(100) NOT NULL,
     duration FLOAT NOT NULL,
     year INT NOT NULL
 )
 """
 create_staging_events = """
 CREATE TABLE IF NOT EXISTS staging_events(
-    artist VARCHAR(40) NOT NULL,
-    auth VARCHAR(50) NOT NULL,
-    first_name VARCHAR(20) NOT NULL,
+    artist VARCHAR(100) NOT NULL,
+    auth VARCHAR(100) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
     gender CHAR(1) NOT NULL,
     item_in_session INT NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     length FLOAT NOT NULL,
     level VARCHAR(5) NOT NULL,
     location VARCHAR(100) NOT NULL,
     method VARCHAR(5),
-    page VARCHAR(10),
+    page VARCHAR(20),
     registration varchar(50),
     session_id int,
-    song VARCHAR(50) NOT NULL,
+    song VARCHAR(100) NOT NULL,
     status INT,
     ts BIGINT,
     user_agent VARCHAR(100),
@@ -65,7 +75,7 @@ CREATE TABLE IF NOT EXISTS songplays(
 create_song = """
 CREATE TABLE IF NOT EXISTS songs(
     song_id VARCHAR(25) PRIMARY KEY,
-    title VARCHAR(50) NOT NULL,
+    title VARCHAR(100) NOT NULL,
     artist_id VARCHAR(25) NOT NULL,
     year INT NOT NULL,
     duration FLOAT NOT NULL
@@ -115,4 +125,24 @@ create_table_queries = [
 ]
 # endregion
 
+# copy data from s3 to staging area
+copy_staging_songs = """
+copy staging_songs from 's3://udacity-dend/song_data'
+iam_role '{}'
+format as json 'auto'
+region 'us-west-2';
+""".format(
+    DWH_ROLE_ARN
+)
+
+copy_staging_events = """
+    copy staging_events from 's3://udacity-dend/log_data'
+iam_role '{}'
+format as json 'auto'
+region 'us-west-2';
+""".format(
+    DWH_ROLE_ARN
+)
+
+copy_queries = [copy_staging_songs]
 # TODO: Add insert table query for fact and dimension table
